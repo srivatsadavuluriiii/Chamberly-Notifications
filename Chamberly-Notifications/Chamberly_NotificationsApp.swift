ef49be962023c1a5887298025cb743815999e3df
srivatsadavuluriiii <xstcy@protonmail.com>
2024-03-12 22:06:11 +0530

    Initial Commit

---
+++ Chamberly-Notifications/Chamberly_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chamberly_NotificationsApp.swift
+//  Chamberly-Notifications
+//
+//  Created by srivatsa davuluri on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Chamberly_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
